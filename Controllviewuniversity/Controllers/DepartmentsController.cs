9905aac199d3bd22e033fa2446940f4db53a6dbd
christoferkrabbi <Christoferkrabbi@gmail.com>
2024-10-03 13:01:27 +0300

    Delete

--- Controllviewuniversity/Controllers/DepartmentsController.cs
+++ Controllviewuniversity/Controllers/DepartmentsController.cs
@@ -63,44 +63,44 @@ namespace ContosoUniversity.Controllers
             return View(department);
         }
 
-        //Delete GET meetod, otsib andmebaasist kaasaantud id järgi osakonda.
+        Delete GET meetod, otsib andmebaasist kaasaantud id järgi osakonda.
 
-        //[HttpGet]
-        //public async Task<IActionResult> Delete(int? id)
-        //{
-        //    if (id == null) // Kui id on tühi/null, siis osakonda ei leia
-        //    {
-        //        return NotFound();
-        //    }
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) // Kui id on tühi/null, siis osakonda ei leia
+            {
+                return NotFound();
+            }
 
-        //    string query = "SELECT * FROM Departments WHERE DepartmentID = {0}"; // Tehakse osakonna objekt, andmebaasis oleva ID järgi
-        //    var department = await _context.Departments
-        //        .FromSqlRaw(query, id)
-        //        .Include(d => d.Administrator)
-        //        .AsNoTracking()
-        //        .FirstOrDefaultAsync();
+            string query = "SELECT * FROM Departments WHERE DepartmentID = {0}"; // Tehakse osakonna objekt, andmebaasis oleva ID järgi
+            var department = await _context.Departments
+                .FromSqlRaw(query, id)
+                .Include(d => d.Administrator)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
 
-        //    if (department == null) // Kui õpetaja objekt on tühi/null, siis ka õpetajat ei leita
-        //    {
-        //        return NotFound();
-        //    }
+            if (department == null) // Kui õpetaja objekt on tühi/null, siis ka õpetajat ei leita
+            {
+                return NotFound();
+            }
 
-        //    return View(department);
-        //}
+            return View(department);
+        }
 
-        //// Delete POST meetod, teostab andmebaasis vajaliku muudatuse. Ehk kustutab andme ära
+        // Delete POST meetod, teostab andmebaasis vajaliku muudatuse. Ehk kustutab andme ära
 
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> DeleteConfirmed(int id)
-        //{
-        //    var department = await _context.Departments.FindAsync(id); //Otsime andmebaasist osakonda id järgi, ja paneme ta "osakond" nimelisse muutujasse.
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var department = await _context.Departments.FindAsync(id); //Otsime andmebaasist osakonda id järgi, ja paneme ta "osakond" nimelisse muutujasse.
 
-        //    _context.Departments.Remove(department);
-        //    await _context.SaveChangesAsync();
+            _context.Departments.Remove(department);
+            await _context.SaveChangesAsync();
 
-        //    return RedirectToAction(nameof(Index));
-        //}
+            return RedirectToAction(nameof(Index));
+        }
 
         // Edit GET
 
