7b5672b9eeac1f0ba9cee10f8cc206ddbfffbf52
Christoferkrabbi <160104752+Christoferkrabbi@users.noreply.github.com>
2024-10-21 13:55:27 +0300

    Added DetailsDelete View and controller, deleted Delete and Details Views

--- Controllviewuniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ Controllviewuniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d46422e3b79dd7679172088da3622c47da0ce747")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+248af777c9d3cd5f08a89f0da06ed74083be68c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
