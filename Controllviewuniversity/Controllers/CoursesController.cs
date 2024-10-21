392ef675b90525fa9fb93e7e92cd69f2d0678b80
Christoferkrabbi <160104752+Christoferkrabbi@users.noreply.github.com>
2024-10-21 13:14:05 +0300

    CreateEdit

--- Controllviewuniversity/Controllers/CoursesController.cs
+++ Controllviewuniversity/Controllers/CoursesController.cs
@@ -23,46 +23,7 @@ namespace ContosoUniversity.Controllers
             return View(await schoolContext.ToListAsync());
         }
 
-        //public async Task<IActionResult> Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var course = await _context.Courses
-        //        .FirstOrDefaultAsync(m => m.CourseID == id);
-        //    if (course == null) { return NotFound(); }
-
-        //    ViewData["Action"] = "Details";
-        //    return View("DetailsDelete", course);
-        //}
-
-        //public async Task<IActionResult> Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var course = await _context.Courses
-        //        .FirstOrDefaultAsync(m => m.CourseID == id);
-
-        //    if (course == null) { return NotFound(); }
-
-        //    ViewData["Action"] = "Delete";
-        //    return View("DetailsDelete", course);
-        //}
-
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> DeleteConfirmed(int id)
-        //{
-        //    var course = await _context.Courses.FindAsync(id);
-        //    _context.Courses.Remove(course);
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction(nameof(Index));
-        //}
+       
 
         public async Task<IActionResult> Clone(int? id)
         {
@@ -219,6 +180,128 @@ namespace ContosoUniversity.Controllers
             await _context.SaveChangesAsync();
 
             return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Clone(int id)
+        {
+            var course = await _context.Courses
+                .FirstOrDefaultAsync(c => c.CourseID == id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            var maxCourseID = await _context.Courses.MaxAsync(c => c.CourseID);
+            var newCourseID = maxCourseID + 1;
+
+            var clonedCourse = new Course
+            {
+                CourseID = newCourseID,
+                Title = course.Title,
+                Credits = course.Credits
+            };
+
+            _context.Courses.Add(clonedCourse);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateEdit(int? id)
+        {
+            if (id != null)
+            {
+                ViewBag.mode2 = "edit";
+                var course = await _context.Courses
+                    .FirstOrDefaultAsync(c => c.CourseID == id);
+
+                if (course == null)
+                {
+                    return NotFound();
+                }
+
+                return View(course);
+            }
+            else
+            {
+                ViewBag.mode2 = "create";
+
+                return View();
+            }
+        }
+
+        [HttpPost, ActionName("CreateEdit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateEdit(int? id, [Bind("CourseID,Title,Credits")] Course course)
+        {
+            if (id != null)
+            {
+                ViewBag.mode2 = "edit";
+            }
+            else
+            {
+                ViewBag.mode2 = "create";
+            }
+            if (ModelState.IsValid)
+            {
+                if (id != null)
+                {
+                    var existingCourse = await _context.Courses
+                    .FirstOrDefaultAsync(c => c.CourseID == id);
+
+                    if (existingCourse == null)
+                    {
+                        return NotFound();
+                    }
+
+                    if (course.CourseID < 0)
+                    {
+                        ModelState.AddModelError("CourseID", "CourseID is negative. Please enter a positive CourseID.");
+                        return View(course);
+                    }
+
+                    if (_context.Courses.Any(c => c.CourseID == course.CourseID))
+                    {
+                        if (course.CourseID == existingCourse.CourseID)
+                        {
+                            existingCourse.CourseID = course.CourseID;
+                            existingCourse.Title = course.Title;
+                            existingCourse.Credits = course.Credits;
+
+                            await _context.SaveChangesAsync();
+                            return RedirectToAction("Index");
+                        }
+                        // Add a validation error to the ModelState
+                        ModelState.AddModelError("CourseID", "CourseID already exists. Please enter an unique CourseID.");
+                        return View(course); // Return the same view with the error message
+                    }
+
+                    _context.Courses.Remove(existingCourse);
+                    _context.Courses.Add(course);
+
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    if (_context.Courses.Any(c => c.CourseID == course.CourseID))
+                    {
+                        // Add a validation error to the ModelState
+                        ModelState.AddModelError("CourseID", "CourseID already exists. Please enter an unique CourseID.");
+                        return View(course); // Return the same view with the error message
+                    }
+                    if (course.CourseID < 0)
+                    {
+                        ModelState.AddModelError("CourseID", "CourseID is negative. Please enter a positive CourseID.");
+                        return View(course);
+                    }
+                    _context.Courses.Add(course);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(course);
         }
 
 
--- Controllviewuniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ Controllviewuniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7cc6e7b884ea61b725c83b190ad108d2afcc2fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+563bdbbb06c1a7a8ee843aafc02151c900ba7f90")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
