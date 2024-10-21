d46422e3b79dd7679172088da3622c47da0ce747
Christoferkrabbi <160104752+Christoferkrabbi@users.noreply.github.com>
2024-10-21 13:46:27 +0300

    Added Delete View and Controller for Courses

--- Controllviewuniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ Controllviewuniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af1cccc3050365dbe49422a79ce754e513c379f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7343d845c995b1905b702eeb1fce407ae69d8cb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
