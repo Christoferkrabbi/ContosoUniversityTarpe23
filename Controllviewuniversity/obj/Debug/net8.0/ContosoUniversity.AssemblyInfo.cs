af1cccc3050365dbe49422a79ce754e513c379f5
Christoferkrabbi <160104752+Christoferkrabbi@users.noreply.github.com>
2024-10-21 13:37:07 +0300

    Added index And Controller for courses

--- Controllviewuniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ Controllviewuniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64a68edeb2e4ec035b707eb848cedaead7ce5163")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8530808dcc4529383a66544529fac3a2b78dd959")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
